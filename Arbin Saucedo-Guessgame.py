46b17f6181d27506b05e9bce3a7ed77ec95dba39
Nmph24 <34071840+Nmph24@users.noreply.github.com>
2017-12-04 10:48:42 -0800

    Added Function
    12.4.2017

--- Arbin Saucedo-Guessgame.py
+++ Arbin Saucedo-Guessgame.py
@@ -1,1 +1,10 @@
-# Arbin Saucedo
+import random
+# 1) Generate random number
+# 2) Take on input (number) from the user
+# 3) Compare input to generated number
+# 4) Add "Higher" and "Lower" statements
+# 5) Add 5 guesses
+
+print(random.randint(0, 50))
+
+
---
+++ WarmUps.py
@@ -0,0 +1,8 @@
+def reverse_order(lastname, firstname):
+    # print("%s, %s" % (lastname, firstname))
+    print(lastname + " " + firstname) # Concatenation
+
+def reverse_orders():
+    first_name = input ("First Name")
+    last_name = input("Last Name")
+    print(print("%s, %s" % (last_name, first_name)))
