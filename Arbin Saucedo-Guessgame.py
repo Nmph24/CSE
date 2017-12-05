b6a35a2e94f7cea596aeaa6685b3a7cfe3541663
Nmph24 <34071840+Nmph24@users.noreply.github.com>
2017-12-05 10:49:57 -0800

    Added Function
    12.5.17

--- Arbin Saucedo-Guessgame.py
+++ Arbin Saucedo-Guessgame.py
@@ -1,10 +1,46 @@
 import random
 # 1) Generate random number
-# 2) Take on input (number) from the user
+# 2) Take on input (number) from the user/ Ask The user for an input
 # 3) Compare input to generated number
 # 4) Add "Higher" and "Lower" statements
 # 5) Add 5 guesses
 
-print(random.randint(0, 50))
+# print(random.randint(0, 50))
+
+
+# def random_number(number):
+#    print(random.randint(0, 50))
+
+
+#def guess(number):
+    # if number >= 60:
+    #     return "Number Too High"
+    # elif number >= 60:
+    #     return "Number Too High"
+    # elif number == 60:
+    #     return "Correct"
+    # elif number >= 60:
+    #     return "Number too Low"
+    # else:
+    #     return "Number too Low"
+
+
+# guess = input(" ")
+# print("Is The Number %s?" % guess)
+
+random = random.randint(1, 50)
+
+def guess(number_guess):
+    if number_guess >= random:
+        return "Number is too High"
+    elif number_guess == random:
+        return "Number Is Correct"
+    elif number_guess <= random:
+        return "Number is too Low"
+
+
+print(guess(10))
+# guessNum = input (" ")
+# print("Is number %s" % guessNum)
 
 
--- WarmUps.py
+++ WarmUps.py
@@ -6,3 +6,7 @@ def reverse_orders():
     first_name = input ("First Name")
     last_name = input("Last Name")
     print(print("%s, %s" % (last_name, first_name)))
+# 12.5.17
+def add_py(name):
+    print("%s.py" % name) # Solution 1
+    print(name + ".py") # Solution 2
